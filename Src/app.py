3ad226f36261daaa23243b1eb68d665ed7b220d3
Dimas Arya Ramadhan <dimasaryaramadhan6@gmail.com>
2026-06-20 23:16:06 +0700

    Fix comments formatting in XAUUSDForecasterLSTM class

--- Src/app.py
+++ Src/app.py
@@ -102,7 +102,7 @@ hr {
 # MODEL LSTM
 # -------------------------------------------------------------------
 class XAUUSDForecasterLSTM(nn.Module):
-    # Inisialisasi layer
+    #Inisialisasi layer
     def __init__(self, input_dim=1, hidden_dim=64, num_layers=2, output_dim=1):
         super(XAUUSDForecasterLSTM, self).__init__()
         self.hidden_dim = hidden_dim
@@ -110,7 +110,7 @@ class XAUUSDForecasterLSTM(nn.Module):
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
         
-    # Forward pass
+    #Forward pass
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
